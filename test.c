0b61431f64449e238ff8a017f80e7a785fd032a3
Matthias Sars <msars@student.42berlin.de>
2026-05-12 19:45:45 +0200

    updated libftprintf

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: msars <msars@student.42berlin.de>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/08 20:51:01 by msars             #+#    #+#             */
-/*   Updated: 2026/05/12 18:57:59 by msars            ###   ########.fr       */
+/*   Updated: 2026/05/12 19:21:31 by msars            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,5 @@ int	main(void)
 	ft_printf("Hello world!\n");
 	ft_printf("Donald's nephews are: %s, %s, and %s.\n", "Huey", "Dewey", "Louie");
 	ft_printf("Answer to the Ultimate Question of Life, the Universe, and Everything: %d!\n", 42);
-	ft_printf("%d%% %c %c %c\n", 100, 'a', 'b', 'c');
+	ft_printf("%i%% %c %c %c\n", 100, 'a', 'b', 'c');
 }
